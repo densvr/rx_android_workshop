b6a0fb174229565f89f454f9f1723a360be9dedb
Daniel Sergeev <danser@yandex-team.ru>
2019-12-14 16:46:43 +0300

    add schedulers sample for slideshow

--- app/src/main/java/com/danser/meetup_2019/samples/Schedulers.kt
+++ app/src/main/java/com/danser/meetup_2019/samples/Schedulers.kt
@@ -3,10 +3,34 @@ package com.danser.meetup_2019.samples
 import java.lang.Thread.currentThread
 
 fun loadItems() {
-    getItemsFromRemoteSource()
-        .doOnNext { item -> Log.d(TAG, "Emitting item $item on thread ${currentThread().name}") }
+    uploadRepository.uploadImage(image: Image)
+        .doOnNext { (image: Image, progress: Int) ->
+            Log.d(TAG, "uploading on thread ${currentThread().name}")
+        }
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
-        .doOnNext { item -> Log.d(TAG, "Consuming item $item on thread ${currentThread().name}") }
-        .subscribe { item -> showItem(item) }
+        .doOnNext { item ->
+            showBlurredImage(image)
+            Log.d(TAG, "Showing image on thread ${currentThread().name}")
+        }
+        .observeOn(Schedulers.background())
+        .flatMapSingle { (image, progress) ->
+            if (progress == 100) {
+                blurImage(image)
+                    .doOnNext { Log.d(TAG, "show progress on thread ${currentThread().name}") }
+                    .map { blurred -> blurred }
+                    .onErrorReturn { image }
+                    .subscribeOn(Schedulers.computation())
+            } else {
+                Single.just(image)
+            }
+        }
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+            { image ->
+                showBlurredImage(image)
+                Log.d(TAG, "Showing image on thread ${currentThread().name}")
+            },
+            { th -> showError(th) }
+        )
 }
